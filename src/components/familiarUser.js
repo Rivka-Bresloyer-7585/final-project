148023ba32f6b66d41eee512885d213fd642d9fe
ריקי ברסלויער <riki151888@gmail.com>
2021-05-19 01:46:19 +0300

    login page

--- src/components/familiarUser.js
+++ src/components/familiarUser.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import { Button } from '@material-ui/core';
 import { useHistory } from "react-router-dom";
-
+import Login from './login';
 const FamiliarUser = () => {
     
     let history = useHistory();
 
-    const registration = () => {
+    const registration = () => { 
         history.push("/registration");
     }
     return (
         <div>
+            <Login/>
             <Button onClick={registration} variant="contained" color="primary">
                 לרישום לאתר
             </Button>

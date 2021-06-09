73562ae60b40f00ab4693f43e0b0f15088b41706
ריקי ברסלויער <riki151888@gmail.com>
2021-06-09 18:47:17 +0300

    pages from a familier user

---
+++ src/components/familiarUser/cookBook.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+const CookBook=()=>{
+
+    return (
+        <div>CookBook</div>
+    )
+}
+
+export default CookBook;
---
+++ src/components/familiarUser/uploadingRecipe.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+const UploadingRecipe=()=>{
+
+    return (
+        <div>UploadingRecipe</div>
+    )
+}
+
+export default UploadingRecipe;
